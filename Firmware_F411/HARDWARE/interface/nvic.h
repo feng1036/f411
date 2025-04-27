78ca9db6f0eb447e6dc4ab67fb0e8a0a0242a577
ddilemea <116473619+feng1036@users.noreply.github.com>
2025-04-27 21:48:28 +0800

    初步

--- Firmware_F411/HARDWARE/interface/nvic.h
+++
@@ -1,9 +0,0 @@
-#ifndef __NVIC_H
-#define __NVIC_H
-#include "sys.h"
-
-
-void nvicInit(void);
-u32 getSysTickCnt(void);	
-
-#endif /* __NVIC_H */
--- Firmware_F411/OBJ/system_stm32f4xx.d
+++
@@ -1,9 +0,0 @@
-..\obj\system_stm32f4xx.o: system_stm32f4xx.c
-..\obj\system_stm32f4xx.o: stm32f4xx_conf.h
-..\obj\system_stm32f4xx.o: stm32f4xx.h
-..\obj\system_stm32f4xx.o: ..\CORE\core_cm4.h
-..\obj\system_stm32f4xx.o: C:\Keil_v5\ARM\ARM_Compiler_5\Bin\..\include\stdint.h
-..\obj\system_stm32f4xx.o: ..\CORE\core_cmInstr.h
-..\obj\system_stm32f4xx.o: ..\CORE\core_cmFunc.h
-..\obj\system_stm32f4xx.o: ..\CORE\core_cm4_simd.h
-..\obj\system_stm32f4xx.o: system_stm32f4xx.h
